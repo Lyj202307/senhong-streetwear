e42c075a4bf72e445cc9a5c685ffe4394bd41a60
senhong <3496599223@qq.com>
2026-08-04 10:26:20 +0800

    fix: add BrowserRouter basename and SPA 404 fallback

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import Contact from './pages/Contact';
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/senhong-streetwear">
       <ScrollToTop />
       <Navbar />
       <Routes>
